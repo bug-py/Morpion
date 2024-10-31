c8ce6ce0e43f8ffb47b72163b84fbb5daf41cf1a
bug-py <cocorigolo6@gmail.com>
2024-10-31 12:11:58 +0100

    ajout config html

--- public/js/config.js
+++ public/js/config.js
@@ -1,14 +1,22 @@
-let config=JSON.parse(localStorage.getItem("Config"))
+let config=JSON.parse(localStorage.getItem("Players"))
 if(config==null){
     config={
-        Name:"Bleu",
-        Event:[[$("body"),{"background":"rgb(64, 64, 255"}],[$(".jeu"),{"color":"blue"}]],
-        css:{"animation":"Basic 1s","background":"blue"},
-        win:[[$(".tableau *"),{"background":"blue"}]]
+            Player1:{
+               Name:"Bleu",
+               Event:[[$("body"),{"background":"rgb(64, 64, 255"}],[$(".jeu"),{"color":"blue"}]],
+               css:{"animation":"Basic 1s","background":"blue"},
+               win:[[$(".tableau *"),{"background":"blue"}]]
+            },
+            Player2:{
+                Name:"Rouge",
+                Event:[[$("body"),{"background":"rgb(255, 105, 105)"}],[$(".jeu"),{"color":"red"}]],
+                css:{"animation":"Basic 1s","background":"red"},
+                win:[[$(".tableau *"),{"background":"red"}]]
+            }
      }
 }
 $(()=>{
     $(".sauv").on("click",(event)=>{
-        event.target
+        localStorage.setItem("Config",JSON.stringify(config))
     })
 })
